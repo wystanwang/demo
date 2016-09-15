8b4e30c62a5c53c6037e4781d28e9705e511be7c
wystanwang <wang_wang@qq.com>
2016-09-15 21:42:46 +0800

    three

---
+++ haha-dao/src/main/java/com/angelostech/huhuhahei/haha_dao/Aoo6.java
@@ -0,0 +1,8 @@
+package com.angelostech.huhuhahei.haha_dao;
+
+public class Aoo6 {
+	public static void main(String[] args){
+		System.out.println("fix bug complete now");
+		System.out.println("fix bug complete now");
+	}
+}
